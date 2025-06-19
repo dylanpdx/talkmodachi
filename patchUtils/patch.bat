858ffeec281c82ea7c5fb503eaeb428403ad6259
Dylan <dylanpdx@gmail.com>
2025-06-19 02:27:14 +0100

    Add EU hooks

--- patchUtils/patch.bat
+++ patchUtils/patch.bat
@@ -1,5 +1,13 @@
-3dstool.exe -xvtf cxi .\US_orig.cxi --header header --exefs exefs --exh exheader.bin --logo logo --plain plain --romfs romfs
+3dstool.exe -xvtf cxi %* --header header --exefs exefs --exh exheader.bin --logo logo --plain plain --romfs romfs
 3dstool.exe -xvtf exefs .\exefs --exefs-dir exefsd --header exfsheader
 pause
 3dstool.exe -cvtf exefs .\exefs --exefs-dir exefsd --header exfsheader
-3dstool.exe -cvtf cxi .\US_patched.cxi --header header --exefs exefs --exh exheader.bin --logo logo --plain plain --romfs romfs --not-encrypt
+3dstool.exe -cvtf cxi .\patched.cxi --header header --exefs exefs --exh exheader.bin --logo logo --plain plain --romfs romfs --not-encrypt
+del plain
+del romfs
+del exefs
+del exfsheader
+del header
+del logo
+del exheader.bin
+rmdir /s /q exefsd
